1611fce03eb5a671b9d0fc42299edcd834b45ea9
Evan D. Hoffman <evandhoffman@gmail.com>
2012-01-31 17:41:19 -0500

    Bunch of constructors.

--- src/com/evanhoffman/sunrise/SunPosition.java
+++ src/com/evanhoffman/sunrise/SunPosition.java
@@ -1,96 +1,182 @@
 package com.evanhoffman.sunrise;
 
+import java.util.Date;
+
 /**
- * Blatantly stolen from http://stackoverflow.com/questions/257717/position-of-the-sun-given-time-of-day-and-lat-long
- * <pre>
- * sunPosition <- function(year, month, day, hour=12, min=0, sec=0,
-                        lat=46.5, long=6.5) {
-  twopi <- 2 * pi
-  deg2rad <- pi / 180
+ * Blatantly stolen from http://stackoverflow.com/questions/257717/position-of-the-sun-given-time-of-day-and-lat-long and
+ * http://stackoverflow.com/questions/8708048/position-of-the-sun-given-time-of-day-latitude-and-longitude/8764866#8764866
+ * 
+<pre>
+sunPosition <- function(year, month, day, hour=12, min=0, sec=0,
+                    lat=46.5, long=6.5) {
 
-  # Get day of the year, e.g. Feb 1 = 32, Mar 1 = 61 on leap years
-  month.days <- c(0,31,28,31,30,31,30,31,31,30,31,30)
-  day <- day + cumsum(month.days)[month]
-  leapdays <- year %% 4 == 0 & (year %% 400 == 0 | year %% 100 != 0) & day >= 60
-  day[leapdays] <- day[leapdays] + 1
+    twopi <- 2 * pi
+    deg2rad <- pi / 180
 
-  # Get Julian date - 2400000
-  hour <- hour + min / 60 + sec / 3600 # hour plus fraction
-  delta <- year - 1949
-  leap <- trunc(delta / 4) # former leapyears
-  jd <- 32916.5 + delta * 365 + leap + day + hour / 24
+    # Get day of the year, e.g. Feb 1 = 32, Mar 1 = 61 on leap years
+    month.days <- c(0,31,28,31,30,31,30,31,31,30,31,30)
+    day <- day + cumsum(month.days)[month]
+    leapdays <- year %% 4 == 0 & (year %% 400 == 0 | year %% 100 != 0) & 
+                day >= 60 & !(month==2 & day==60)
+    day[leapdays] <- day[leapdays] + 1
 
-  # The input to the Atronomer's almanach is the difference between
-  # the Julian date and JD 2451545.0 (noon, 1 January 2000)
-  time <- jd - 51545.
+    # Get Julian date - 2400000
+    hour <- hour + min / 60 + sec / 3600 # hour plus fraction
+    delta <- year - 1949
+    leap <- trunc(delta / 4) # former leapyears
+    jd <- 32916.5 + delta * 365 + leap + day + hour / 24
 
-  # Ecliptic coordinates
+    # The input to the Atronomer's almanach is the difference between
+    # the Julian date and JD 2451545.0 (noon, 1 January 2000)
+    time <- jd - 51545.
 
-  # Mean longitude
-  mnlong <- 280.460 + .9856474 * time
-  mnlong <- mnlong %% 360
-  mnlong[mnlong < 0] <- mnlong[mnlong < 0] + 360
+    # Ecliptic coordinates
 
-  # Mean anomaly
-  mnanom <- 357.528 + .9856003 * time
-  mnanom <- mnanom %% 360
-  mnanom[mnanom < 0] <- mnanom[mnanom < 0] + 360
-  mnanom <- mnanom * deg2rad
+    # Mean longitude
+    mnlong <- 280.460 + .9856474 * time
+    mnlong <- mnlong %% 360
+    mnlong[mnlong < 0] <- mnlong[mnlong < 0] + 360
 
-  # Ecliptic longitude and obliquity of ecliptic
-  eclong <- mnlong + 1.915 * sin(mnanom) + 0.020 * sin(2 * mnanom)
-  eclong <- eclong %% 360
-  eclong[eclong < 0] <- eclong[eclong < 0] + 360
-  oblqec <- 23.429 - 0.0000004 * time
-  eclong <- eclong * deg2rad
-  oblqec <- oblqec * deg2rad
+    # Mean anomaly
+    mnanom <- 357.528 + .9856003 * time
+    mnanom <- mnanom %% 360
+    mnanom[mnanom < 0] <- mnanom[mnanom < 0] + 360
+    mnanom <- mnanom * deg2rad
 
-  # Celestial coordinates
-  # Right ascension and declination
-  num <- cos(oblqec) * sin(eclong)
-  den <- cos(eclong)
-  ra <- atan(num / den)
-  ra[den < 0] <- ra[den < 0] + pi
-  ra[den >= 0 & num < 0] <- ra[den >= 0 & num < 0] + twopi
-  dec <- asin(sin(oblqec) * sin(eclong))
+    # Ecliptic longitude and obliquity of ecliptic
+    eclong <- mnlong + 1.915 * sin(mnanom) + 0.020 * sin(2 * mnanom)
+    eclong <- eclong %% 360
+    eclong[eclong < 0] <- eclong[eclong < 0] + 360
+    oblqec <- 23.439 - 0.0000004 * time
+    eclong <- eclong * deg2rad
+    oblqec <- oblqec * deg2rad
 
-  # Local coordinates
-  # Greenwich mean sidereal time
-  gmst <- 6.697375 + .0657098242 * time + hour
-  gmst <- gmst %% 24
-  gmst[gmst < 0] <- gmst[gmst < 0] + 24.
+    # Celestial coordinates
+    # Right ascension and declination
+    num <- cos(oblqec) * sin(eclong)
+    den <- cos(eclong)
+    ra <- atan(num / den)
+    ra[den < 0] <- ra[den < 0] + pi
+    ra[den >= 0 & num < 0] <- ra[den >= 0 & num < 0] + twopi
+    dec <- asin(sin(oblqec) * sin(eclong))
 
-  # Local mean sidereal time
-  lmst <- gmst + long / 15.
-  lmst <- lmst %% 24.
-  lmst[lmst < 0] <- lmst[lmst < 0] + 24.
-  lmst <- lmst * 15. * deg2rad
+    # Local coordinates
+    # Greenwich mean sidereal time
+    gmst <- 6.697375 + .0657098242 * time + hour
+    gmst <- gmst %% 24
+    gmst[gmst < 0] <- gmst[gmst < 0] + 24.
 
-  # Hour angle
-  ha <- lmst - ra
-  ha[ha < -pi] <- ha[ha < -pi] + twopi
-  ha[ha > pi] <- ha[ha > pi] - twopi
+    # Local mean sidereal time
+    lmst <- gmst + long / 15.
+    lmst <- lmst %% 24.
+    lmst[lmst < 0] <- lmst[lmst < 0] + 24.
+    lmst <- lmst * 15. * deg2rad
 
-  # Latitude to radians
-  lat <- lat * deg2rad
+    # Hour angle
+    ha <- lmst - ra
+    ha[ha < -pi] <- ha[ha < -pi] + twopi
+    ha[ha > pi] <- ha[ha > pi] - twopi
 
-  # Azimuth and elevation
-  el <- asin(sin(dec) * sin(lat) + cos(dec) * cos(lat) * cos(ha))
-  az <- asin(-cos(dec) * sin(ha) / cos(el))
-  elc <- asin(sin(dec) / sin(lat))
-  az[el >= elc] <- pi - az[el >= elc]
-  az[el <= elc & ha > 0] <- az[el <= elc & ha > 0] + twopi
+    # Latitude to radians
+    lat <- lat * deg2rad
 
-  el <- el / deg2rad
-  az <- az / deg2rad
-  lat <- lat / deg2rad
+    # Azimuth and elevation
+    el <- asin(sin(dec) * sin(lat) + cos(dec) * cos(lat) * cos(ha))
+    az <- asin(-cos(dec) * sin(ha) / cos(el))
 
-  return(list(elevation=el, azimuth=az))
+    # For logic and names, see Spencer, J.W. 1989. Solar Energy. 42(4):353
+    cosAzPos <- (0 <= sin(dec) - sin(el) * sin(lat))
+    sinAzNeg <- (sin(az) < 0)
+    az[cosAzPos & sinAzNeg] <- az[cosAzPos & sinAzNeg] + twopi
+    az[!cosAzPos] <- pi - az[!cosAzPos]
+
+    # if (0 < sin(dec) - sin(el) * sin(lat)) {
+    #     if(sin(az) < 0) az <- az + twopi
+    # } else {
+    #     az <- pi - az
+    # }
+
+
+    el <- el / deg2rad
+    az <- az / deg2rad
+    lat <- lat / deg2rad
+
+    return(list(elevation=el, azimuth=az))
 }
 </pre>
  * @author evan
  *
  */
 public class SunPosition {
+	private double elevation = 0;
+	private double azimuth = 0;
+	private Date date = null;
+	private double latitude = 0;
+	private double longitude = 0;
+	
+	public SunPosition(Date d, double latitude, double longitude) {
+		if (d == null || latitude > 360 || longitude > 360) {
+			throw new IllegalArgumentException();
+		}
+		this.date = d;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		calculatePosition();
+	}
+	
+	/**
+	 * Do the work here.
+	 */
+	private void calculatePosition() {
+		
+	}
+	
+	public void calculatePosition(Date d, double latitude, double longitude) {
+		if (d == null || latitude > 360 || longitude > 360) {
+			throw new IllegalArgumentException();
+		}
+		this.date = d;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		calculatePosition();
+	}
+	
+	public void calculatePosition(Date d) {
+		if (d == null) {
+			throw new IllegalArgumentException();
+		}
+		this.date = d;		
+		calculatePosition();
+	}
+	
+	public void calculatePosition(double latitude, double longitude) {
+		if (latitude > 360 || longitude > 360) {
+			throw new IllegalArgumentException();
+		}
+		this.latitude = latitude;
+		this.longitude = longitude;
+		calculatePosition();		
+	}
+	
+	public double getElevation() {
+		return elevation;
+	}
 
+	public double getAzimuth() {
+		return azimuth;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+	
+	
 }
